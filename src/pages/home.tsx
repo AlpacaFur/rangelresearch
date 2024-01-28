cfc1da95fcb42131f1e81c0371fb138d34403c33
Luke Taylor <luke@bigring.org>
2024-01-27 19:22:49 -0500

    Remove default bg color

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,16 +8,16 @@ export default function Home() {
     <div className="bg-white">
       <NavigationBar />
 
-      <div className="mt-6 bg-transparent w-full flex items-center px-5">
-        <div className="rounded bg-gradient-to-r from-brown1 to-brown2 w-full items-center flex justify-center mr-3">
-          <h1 className="text-brown5 text-6xl w-1/2 m-auto bg-transparent font-julius title font-bold text-center">
+      <div className="mt-6 w-full flex items-center">
+        <div className="rounded bg-gradient-to-r from-brown1 to-brown2 w-full items-center flex justify-center mr-3 px-5 py-5">
+          <h1 className="text-brown5 text-6xl w-1/2 m-auto font-julius title font-bold text-center">
             Families and Schools Research Project
           </h1>
           <div className="rounded bg-brown3 w-1/3 grid items-center justify-center mx-7">
-            <h1 className="text-4xl text-gray-purple w-84 bg-transparent font-bold pb-5">
+            <h1 className="text-4xl text-gray-purple w-84 font-bold pb-5">
               Qualtrics Interest Form
             </h1>
-            <div className="bg-transparent flex justify-center">
+            <div className=" flex justify-center">
               <QualtricsButton />
             </div>
           </div>
@@ -25,8 +25,8 @@ export default function Home() {
       </div>
 
       <div className="bg-light-gray mt-6 w-full flex items-center">
-        <div className="rounded bg-transparent w-1/3 items-center grid justify-center mr-3">
-          <h1 className="text-3xl w-84 bg-transparent text-gray-purple font-bold">
+        <div className="rounded w-1/3 items-center grid justify-center mr-3">
+          <h1 className="text-3xl w-84 text-gray-purple font-bold">
             The Project
           </h1>
           <div>
@@ -34,8 +34,8 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="rounded bg-transparent w-2/3 grid items-center justify-center">
-          <h1 className="text-slate-800 text-xl w-100 bg-transparent mr-10">
+        <div className="rounded w-2/3 grid items-center justify-center">
+          <h1 className="text-slate-800 text-xl w-100 mr-10">
             Welcome to the{" "}
             <text className="font-semibold">Families and Schools Project</text>,
             a research study exploring the collaborative relationships between
@@ -50,8 +50,8 @@ export default function Home() {
       </div>
 
       <div className="bg-light-gray w-full flex items-center justify-center">
-        <div className="rounded bg-transparent border-4 border-brown5 w-full p-5 mx-16 mb-16">
-          <p className="text-slate-800 text-xl w-100 bg-transparent mb-5">
+        <div className="rounded border-4 border-brown5 w-full p-5 mx-16 mb-16">
+          <p className="text-slate-800 text-xl w-100 mb-5">
             The voices of parents and educators are at the heart of our
             research. By conducting comprehensive interviews with parents, we
             aim to gain nuanced insights into their experiences and perceptions
@@ -59,7 +59,7 @@ export default function Home() {
             faculty and administrators provide us with diverse perspectives on
             the role and impact of parental involvement.
           </p>
-          <p className="text-slate-800 text-xl w-100 bg-transparent">
+          <p className="text-slate-800 text-xl w-100">
             In exploring these dynamics, our project ultimately seeks to
             identify effective strategies that enhance student learning
             experiences and understand the practices that support academic and
@@ -69,34 +69,34 @@ export default function Home() {
           </p>
         </div>
       </div>
-      <div className="my-10 inline-block bg-transparent w-full justify-center">
-        <div className="flex items-center justify-center bg-transparent w-full">
+      <div className="my-10 inline-block w-full justify-center">
+        <div className="flex items-center justify-center w-full">
           <img src="/photos/home1.png" className="" alt="Home Image" />
         </div>
       </div>
 
-      <div className="my-10 inline-block bg-transparent w-full justify-center">
+      <div className="my-10 inline-block w-full justify-center">
         <div className="m-auto bg-brown1 w-96 pt-7 flex justify-center rounded">
-          <h1 className="text-3xl text-gray-purple w-84 bg-transparent font-bold">
+          <h1 className="text-3xl text-gray-purple w-84 font-bold">
             Our Research Values
           </h1>
         </div>
 
-        <div className="flex bg-transparent mt-10 w-full">
+        <div className="flex mt-10 w-full">
           <div className="mx-5 bg-brown1 w-full inline-block justify-center rounded p-8">
             <div className="m-auto rounded-full w-16 bg-brown4 flex justify-center">
-              <h1 className="bg-transparent mt-3 font-bold text-3xl text-brown5">
+              <h1 className=" mt-3 font-bold text-3xl text-brown5">
                 1.
               </h1>
             </div>
 
             <div className="my-5 m-auto bg-white bg-opacity-75 w-auto px-10 border-2 border-brown5 pt-2 flex justify-center rounded">
-              <h1 className="text-xl text-gray-purple w-84 bg-transparent font-bold">
+              <h1 className="text-xl text-gray-purple w-84 font-bold">
                 Transparency
               </h1>
             </div>
 
-            <h1 className="text-brown5 w-84 bg-transparent text-center">
+            <h1 className="text-brown5 w-84 text-center">
               We value open and clear communication with the parents, educators,
               and schools we partner with. Participants will be in direct
               contact with research team members and choose how much information
@@ -106,18 +106,18 @@ export default function Home() {
 
           <div className="mx-5 bg-brown2 w-full inline-block justify-center rounded p-8">
             <div className="m-auto rounded-full w-16 bg-brown4 flex justify-center">
-              <h1 className="bg-transparent mt-3 font-bold text-3xl text-brown5">
+              <h1 className=" mt-3 font-bold text-3xl text-brown5">
                 2.
               </h1>
             </div>
 
             <div className="bg-opacity-75 my-5 m-auto bg-white w-auto px-10 border-2 border-brown5 pt-2 flex justify-center rounded">
-              <h1 className="text-xl text-gray-purple bg-transparent font-bold">
+              <h1 className="text-xl text-gray-purple font-bold">
                 Security & Privacy
               </h1>
             </div>
 
-            <h1 className="text-brown5 w-84 bg-transparent text-center">
+            <h1 className="text-brown5 w-84 text-center">
               We value protecting participant information and maintaining
               privacy. We take several steps to ensure participants&apos;
               confidentiality in adherence to Brown University&apos;s
@@ -127,18 +127,18 @@ export default function Home() {
 
           <div className="mx-5 bg-brown3 w-full inline-block justify-center rounded p-8">
             <div className="m-auto rounded-full w-16 bg-brown4 flex justify-center">
-              <h1 className="bg-transparent mt-3 font-bold text-3xl text-brown5">
+              <h1 className=" mt-3 font-bold text-3xl text-brown5">
                 3.
               </h1>
             </div>
 
             <div className="bg-opacity-75 my-5 m-auto bg-white w-auto px-10 border-2 border-brown5 pt-2 flex justify-center rounded">
-              <h1 className="text-xl text-gray-purple w-84 bg-transparent font-bold">
+              <h1 className="text-xl text-gray-purple w-84 font-bold">
                 Catalyzing Positive Change
               </h1>
             </div>
 
-            <h1 className="text-brown5 w-84 bg-transparent text-center">
+            <h1 className="text-brown5 w-84 text-center">
               We value our partnerships with participants as foundations for
               studying and building school communities that better serve
               students, families, and educators.
